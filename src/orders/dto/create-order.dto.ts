30dcb30ae902d519cddb93551ab7b4d182f67d58
juanovni <constantinemurillo@gmail.com>
2026-02-19 23:46:43 -0500

    Update the order-dto

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,4 +1,6 @@
-import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
+import { Type } from "class-transformer";
+import { ArrayMinSize, IsArray, ValidateNested } from "class-validator";
+import { OrderItemDto } from "./order-item.dto";
 
 enum OrderStatus {
   PENDING = 'PENDING',
@@ -14,21 +16,30 @@ const OrderStatusList = [
 
 export class CreateOrderDto {
 
-  @IsNumber()
-  @IsPositive()
-  totalAmount: number;
+  /*   @IsNumber()
+    @IsPositive()
+    totalAmount: number;
+  
+    @IsNumber()
+    @IsPositive()
+    totalItems: number;
+  
+    @IsEnum(OrderStatusList, {
+      message: `Posible status values are ${OrderStatusList}`
+    })
+    @IsOptional()
+    status: OrderStatus = OrderStatus.PENDING;
+  
+    @IsBoolean()
+    @IsOptional()
+    paid: boolean = false; */
 
-  @IsNumber()
-  @IsPositive()
-  totalItems: number;
 
-  @IsEnum(OrderStatusList, {
-    message: `Posible status values are ${OrderStatusList}`
-  })
-  @IsOptional()
-  status: OrderStatus = OrderStatus.PENDING;
-
-  @IsBoolean()
-  @IsOptional()
-  paid: boolean = false;
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(
+    () => OrderItemDto
+  )
+  items: OrderItemDto[]
 }
