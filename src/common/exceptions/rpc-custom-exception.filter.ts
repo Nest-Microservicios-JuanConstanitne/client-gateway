e670c38903cb0ae88da0e9cde2f19520c3778552
juanovni <constantinemurillo@gmail.com>
2026-03-01 23:45:23 -0500

    Added the auth-service microservice

--- src/common/exceptions/rpc-custom-exception.filter.ts
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -13,6 +13,13 @@ export class RpcCustomExceptionFilter implements ExceptionFilter {
 
     const rpcError = exception.getError();
 
+    if (rpcError.toString().includes('Empty response')) {
+      return response.status(500).json({
+        status: 500,
+        message: rpcError.toString().substring(0, rpcError.toString().indexOf('(') - 1)
+      })
+    }
+
     // Si el error es un objeto
     if (typeof rpcError === 'object') {
 
